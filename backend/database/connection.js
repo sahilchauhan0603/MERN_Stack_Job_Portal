ccfb18278d08b531e2515363ee4be5ac514fd928
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2025-01-12 19:22:39 +0530

    changes

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export const connection = ()=>{
     mongoose.connect(process.env.MONGO_URI, {
-        dbName: "JOB_PORTAL"
     }).then(()=>{
         console.log("Connected to database.")
     }).catch(err=>{
